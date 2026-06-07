1ea317e8db2c417839ef5c9b41d8de972773877c
DIEGOSH-BLACK <diegosh84@gmail.com>
2026-06-06 18:38:50 -0600

    Se agregan archivos y se hace clase de alert, prompt y confirm

---
+++ 01-FUNDAMENTOS/assets/js/alerts.js
@@ -0,0 +1,13 @@
+
+// ALERT
+// alert('Hola Mundo');
+
+
+// PROMPT
+//let nombre = prompt('¿Cuál es tu nombre?', 'Tu nombre');
+//console.log(nombre);
+
+
+// CONFIRM
+let confirmacion = confirm('¿Estás seguro de borrar esto?');
+console.log(confirmacion);
