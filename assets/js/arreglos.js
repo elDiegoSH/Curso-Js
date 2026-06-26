de8218d14723b154dd0b06ad7fbe784aada8f29f
diegoshSistemas <diegosh84@gmail.com>
2026-06-26 11:58:58 -0600

    Practica 1 de arreglos

---
+++ assets/js/arreglos.js
@@ -0,0 +1,21 @@
+let videoJuegos = ['Mario 3', 'Megaman', 'Mario Kart'];
+
+console.log({videoJuegos});
+
+// MOSTRAR PRIMER ELEMENTO
+console.log (videoJuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Diego',
+    1+2,
+    function(){},
+    ()=>{},
+    {a: 1},
+    ['X', 'Megaman', 'Zeero', 'Dr Light', 'Mayimbu', 'Malo']
+];
+
+console.log ( arregloCosas[0] );
+console.log ( arregloCosas[2] );
+console.log ( arregloCosas[7][4] );
